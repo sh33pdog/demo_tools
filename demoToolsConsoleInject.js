c52da9923a5504ed0117e1eec34991509bd1c8bc
sh33pdog <keiron.shepherd@gmail.com>
2022-03-30 11:18:13 +0100

    Create demoToolsConsoleInject.js

---
+++ demoToolsConsoleInject.js
@@ -0,0 +1,5 @@
+javascript: (function(src) {
+    var script = document.createElement("script");
+    script.setAttribute("src", src);
+    document.body.appendChild(script);
+})("https://s3-eu-west-1.amazonaws.com/internaltools/demoTools.js")

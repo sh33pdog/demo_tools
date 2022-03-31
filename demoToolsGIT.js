2915d2198e30fbe595b93da349bed16ed4680985
sh33pdog <keiron.shepherd@gmail.com>
2022-03-31 23:18:16 +0100

    Update demoToolsGIT.js

--- demoToolsGIT.js
+++ demoToolsGIT.js
@@ -2,4 +2,4 @@ javascript: (function(src) {
     var script = document.createElement("script");
     script.setAttribute("src", src);
     document.body.appendChild(script);
-})("https://github.com/sh33pdog/demo_tools/blob/a2100d4e61453eb6e7db86f3aae9d032fbfc0a81/demoToolSrc.js")
+})("https://raw.githubusercontent.com/sh33pdog/demo_tools/main/demoToolSrc.js")
